54c503602fc7630ea082e752be22eaa6fa263cbc
kazuya0077 <kd.jj.0131@gmail.com>
2026-02-04 00:10:48 +0900

    Fix: Cloudflare adapter & Update images/fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
     // Cloudflare Pages用の設定
     site: 'https://pt-kokushi.pages.dev',
-    output: 'static',
+    output: 'server',
+    adapter: cloudflare(),
 });
